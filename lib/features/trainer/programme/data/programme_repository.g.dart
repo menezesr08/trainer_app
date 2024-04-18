400f51d09564fe172b6af915817cd13bb471d5ff
Rohan Menezes <menezesr08@gmail.com>
2024-04-18 09:06:52 +0100

    Create program repository

---
+++ lib/features/trainer/programme/data/programme_repository.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'programme_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$programmeRepositoryHash() =>
+    r'f4038613af7722a008fbca83a57b60b2d73a25d9';
+
+/// See also [programmeRepository].
+@ProviderFor(programmeRepository)
+final programmeRepositoryProvider = Provider<ProgrammeRepository>.internal(
+  programmeRepository,
+  name: r'programmeRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$programmeRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ProgrammeRepositoryRef = ProviderRef<ProgrammeRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
